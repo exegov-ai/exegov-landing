4b1c47a90bb3719b9c4234f7e55f234f3197a089
johnnieskywalker <lwolski0@gmail.com>
2024-04-10 15:14:06 +0200

    feat: added scroll to section

--- src/components/feature-section/index.tsx
+++ src/components/feature-section/index.tsx
@@ -3,7 +3,7 @@ import Check from '@/constants/svg/check.svg';
 
 function FeatureSection() {
   return (
-    <section className={tw(`bg-white pb-6`)}>
+    <section id="features" className={tw(`bg-white pb-6`)}>
       <div className={tw(`max-w-7xl mx-auto p-4 sm:p-6 lg:p-8`)}>
         <div className={tw(`container mx-auto px-6 p-6 bg-white`)}>
           <div className={tw(`mb-16 text-center`)}>
