b39c9fa427a463da5c2dfd7c1efbd546b2fa8a45
johnnieskywalker <lwolski0@gmail.com>
2024-04-11 19:12:55 +0200

    feat: rn

--- src/components/cool-items/index.tsx
+++ src/components/cool-items/index.tsx
@@ -1,12 +1,12 @@
 import { tw } from 'twind';
 
-interface ItemProps {
+interface CoolItemProps {
   title: string;
   description: string;
   Icon: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
 }
 
-function CoolItem({ title, description, Icon }: ItemProps) {
+function CoolItem({ title, description, Icon }: CoolItemProps) {
   return (
     <div className={tw(`w-full border-b md:w-1/2 md:border-r lg:w-1/3 p-8`)}>
       <div className={tw(`flex items-center mb-6`)}>
