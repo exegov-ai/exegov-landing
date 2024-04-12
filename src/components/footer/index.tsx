cfdf160d7a56180700e4fa4e2836e506d12c2fab
johnnieskywalker <lwolski0@gmail.com>
2024-04-12 11:12:27 +0200

    feat: social media footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,33 +3,33 @@ import Facebook from '@/constants/svg/facebook.svg';
 import Linkedin from '@/constants/svg/linkedin.svg';
 import Twitter from '@/constants/svg/twitterx.svg';
 import Github from '@/constants/svg/github.svg';
+import { EXEGOV_BLUE } from '@/constants/values/colors';
 
 function Footer() {
   return (
     <footer className={tw(`bg-white border-t border-gray-400 pt-14 pb-16`)}>
       <div className={tw(`max-w-7xl mx-auto text-gray-400 px-8 lg:px-0 flex flex-wrap`)}>
+        <div className={tw(`ml-auto flex items-center`)} style={{ color: EXEGOV_BLUE, fill: EXEGOV_BLUE }}>
+          <a href="https://www.facebook.com/your-facebook-page" className={tw(`ml-4`)} aria-label="fb">
+            <Facebook />
+          </a>
+          <a href="https://www.linkedin.com/your-linkedin-page" className={tw(`ml-4`)} aria-label="linkedin">
+            <Linkedin />
+          </a>
+          <a href="https://twitter.com/your-ct" className={tw(`ml-4`)} aria-label="x.com">
+            <Twitter />
+          </a>
+          <a href="https://github.com/exegov-ai" className={tw(`ml-4`)} aria-label="GitHub">
+            <Github />
+          </a>
+        </div>
         <div className={tw(`mb-14 flex items-center w-full`)}>
           <img className={tw(`h-12 w-12 mr-4`)} src="logo.svg" alt="logo" width={48} height={48} />
-          <p className={tw(`text-4xl text-indigo-500 font-bold`)} style={{ color: `#4e7a9d` }}>
+          <p className={tw(`text-4xl text-indigo-500 font-bold`)} style={{ color: EXEGOV_BLUE }}>
             Exegov.ai
           </p>
         </div>
       </div>
-      <div className={tw(`ml-auto flex items-center`)}>
-        <a href="https://www.facebook.com/your-facebook-page" className={tw(`ml-4`)} aria-label="fb">
-          <Facebook width={24} height={24} />
-        </a>
-        <a href="https://www.linkedin.com/your-linkedin-page" className={tw(`ml-4`)} aria-label="linkedin">
-          <Linkedin width={24} height={24} />
-        </a>
-        <a href="https://twitter.com/your-ct" className={tw(`ml-4`)} aria-label="x.com">
-          <Twitter width={24} height={24} />
-        </a>
-        <a href="https://github.com/exegov-ai" className={tw(`ml-4`)} aria-label="GitHub">
-          <Github width={24} height={24} />
-        </a>
-      </div>
-      {` `}
     </footer>
   );
 }
