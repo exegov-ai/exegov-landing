8c57fbb18769c3a936984b2509e565d1a801d568
johnnieskywalker <lwolski0@gmail.com>
2024-08-14 17:32:26 +0200

    fix: asfdasffs
    fsf

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,11 @@
 module.exports = {
-    preset: 'ts-jest/presets/js-with-babel',
-    testEnvironment: 'node',
-    transform: {
-      '^.+\\.(ts|tsx)$': 'babel-jest',
-    },
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/src/$1',
-    },
-    transformIgnorePatterns: [
-      '/node_modules/',
-    ],
-  };
-  
+  preset: 'ts-jest/presets/js-with-babel',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'babel-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  transformIgnorePatterns: ['/node_modules/'],
+};
