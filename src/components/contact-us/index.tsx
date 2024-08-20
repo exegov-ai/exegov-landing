780d70331584e003677e9deb8d4dc020c503f6f4
Daniel Studencki <daniel.studencki@gmail.com>
2024-08-20 18:52:50 +0200

    fix: changed the Contact Us section's content
    Fixes #29

--- src/components/contact-us/index.tsx
+++ src/components/contact-us/index.tsx
@@ -7,14 +7,14 @@ function ContactSection() {
       <div className="relative max-w-7xl mx-auto mb-24">
         <div className="overflow-hidden lg:max-w-none lg:flex">
           <div className="py-8 px-6 md:px-0 lg:flex-shrink-1">
-            <h2 className="text-4xl lg:text-7xl font-bold text-gray-800 mb-12 md:px-6 lg:px-8">
-              Get in Touch with Exegov.ai
+            <h2 className="text-4xl lg:text-5xl font-bold text-gray-800 mb-12 md:px-6 lg:px-8">
+              Are you ready to elevate your business with tailored consulting solutions?
             </h2>
             <p className="mt-6 text-base leading-6 text-gray-500 md:px-6 lg:px-8">
-              At Exegov.ai, we believe in the power of collaboration and open communication. Whether you&apos;re
-              navigating the early stages of your startup journey or scaling new heights, your insights and experiences
-              are invaluable to us. We&apos;re committed to creating a platform that not only meets but exceeds your
-              expectations. That&apos;s why we&apos;re always here to listen.
+              At Exegov.ai, were dedicated to helping you achieve your goals. Whether you&apos;re seeking expert advice
+              on scaling your startup or optimizing your operations, we&apos;re here to provide the support you need.
+              Share your challenges and objectives with us, and let&apos;s explore how we can collaborate to drive your
+              success. Reach out today to discover how Exegov.ai can craft the perfect strategy for your business.
             </p>
             <div className="mt-8 md:px-6 lg:px-8">
               <div className="flex items-center">
