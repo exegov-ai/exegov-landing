a9cf35225801276f69fc0b1ce7865b1d09f9bc1d
Daniel Studencki <daniel.studencki@gmail.com>
2024-08-22 18:15:25 +0200

    refactor: removed legacy_pages directory

--- src/pages_legacy/_app.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { AppProps } from 'next/app';
-import '@/styles/global.css';
-import '@fontsource/inter';
-
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
